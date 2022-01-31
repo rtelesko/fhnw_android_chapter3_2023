dedca5bf18df62376225a5b48974ca3a46737128
rainer.telesko <rainer.telesko@fhnw.ch>
2022-01-31 17:38:31 +0100

    New method instead of deprecated startActivityForResult()

--- LotteryInfo/src/main/java/com/example/lotteryinfo/MainActivity.java
+++ LotteryInfo/src/main/java/com/example/lotteryinfo/MainActivity.java
@@ -1,48 +1,52 @@
 package com.example.lotteryinfo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    static final int SECOND_ACTIVITY_REQUEST_CODE = 1;
-
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
     // GUI control
     private TextView tvResult;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Get reference to the TextView
         tvResult = findViewById(R.id.tvResult);
+
+        // New way to send and receive data: startActivityForResult() is deprecated!
+        // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                            Intent data = result.getData();
+                            String selection = data.getStringExtra("SELECTION");
+                            if (selection.equals("Swiss Lottery"))
+                                tvResult.setText("Mode: Choose 6 numbers and 1 lucky number per tip\n" +
+                                        "Draw: Every Wednesday and Saturday");
+                            else
+                                tvResult.setText("Mode: Choose 5 numbers and 2 stars per tip\n" +
+                                        "Draw: Every Tuesday and Friday");
+                        }
+                    }
+                });
     }
 
     public void fetchInfo(View view) {
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        // To receive a result, call startActivityForResult() (instead of startActivity())
-        startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);// Activity is started with requestCode 1
-    }
-
-    // Callback method to get the message from the other Activity
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Check which request we are responding to and if the result is OK
-        if (requestCode == SECOND_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
-            String selection = data.getStringExtra("SELECTION");
-            if (selection.equals("Swiss Lottery"))
-                tvResult.setText("Mode: Choose 6 numbers and 1 lucky number per tip\n" +
-                        "Draw: Every Wednesday and Saturday");
-            else
-                tvResult.setText("Mode: Choose 5 numbers and 2 stars per tip\n" +
-                        "Draw: Every Tuesday and Friday");
-        }
-
+        // Create Intent
+        Intent intent = new Intent(this, SecondActivity.class);
+        someActivityResultLauncher.launch(intent);
     }
 }

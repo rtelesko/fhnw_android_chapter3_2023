1ff7eccbcf1d735244ae7079e352a9efca31acc5
Rainer Telesko <rainer.telesko@gmail.com>
2022-02-08 13:50:22 +0100

    New method instead of deprecated startActivityForResult()

--- LotteryBluetooth/src/main/java/com/example/lotterybluetooth/MainActivity.java
+++ LotteryBluetooth/src/main/java/com/example/lotterybluetooth/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.lotterybluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -44,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else if (!bluetoothAdapter.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             startActivityForResult(turnOn, 0);
             Toast.makeText(getApplicationContext(), "Turned on", Toast.LENGTH_LONG).show();
         } else {
@@ -52,6 +65,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void off(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         bluetoothAdapter.disable();
         Toast.makeText(getApplicationContext(), "Turned off", Toast.LENGTH_LONG).show();
     }
@@ -59,11 +82,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void visible(View v) {
         Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         startActivityForResult(getVisible, 0);
     }
 
 
     public void list(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         pairedDevices = bluetoothAdapter.getBondedDevices();
         ArrayList list = new ArrayList();
         for (BluetoothDevice bt : pairedDevices) list.add(bt.getName());
--- LotteryCamera/src/main/java/com/example/lotterycamera/MainActivity.java
+++ LotteryCamera/src/main/java/com/example/lotterycamera/MainActivity.java
@@ -9,17 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
     // Define the picture id
     private static final int PIC_ID = 123;
-
     // GUI variables
     Button btCamera;
     ImageView ivPhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,50 +32,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btCamera = findViewById(R.id.btCamera);
         ivPhoto = findViewById(R.id.ivPhoto);
-        /*
-        Camera_open button is for open the camera
-        and add the setOnClickListener in this button
-        */
-        btCamera.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                /*
-                Create the camera_intent ACTION_IMAGE_CAPTURE
-                it will open the built-in camera for capturing the image
-                */
-                Intent camera_intent
-                        = new Intent(MediaStore
-                        .ACTION_IMAGE_CAPTURE);
-
-                // Start the activity with camera_intent and request pic id
-                if (camera_intent.resolveActivity(getPackageManager()) != null)
-                    startActivityForResult(camera_intent, PIC_ID);
-                else
-                    Log.d("ImplicitIntent", "Can't handle this!");
-            }
-        });
+        // New way to send and receive data: startActivityForResult() is deprecated!
+        // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                    /*
+                    BitMap is a data structure of image file
+                    which stores the image in memory
+                    Disadvantage: low resolution quality (only thumbnail)
+                    */
+                            Intent data = result.getData();
+                            if (data.getExtras() != null) {     // Check if Intent is empty
+                                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                                // Set the image in ImageView for display
+                                ivPhoto.setImageBitmap(photo);
+                            }
+                        }
+                    }
+                });
     }
 
-    // This method will help to retrieve the image
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
 
-        super.onActivityResult(requestCode, resultCode, data);
-        // Match the request 'pic id' with requestCode and resultCode
-        if (requestCode == PIC_ID && resultCode == RESULT_OK) {
-            /*
-            BitMap is a data structure of image file
-            which stores the image in memory
-            Disadvantage: low resolution quality (only Thumbnail)
-            */
-            if (data.getExtras() != null) {     // Check if Intent is empty
-                Bitmap photo = (Bitmap) data.getExtras().get("data");
-                // Set the image in ImageView for display
-                ivPhoto.setImageBitmap(photo);
-            }
-
-        }
+    public void getPicture(View view) {
+    /* Create Intent
+    Create the camera_intent ACTION_IMAGE_CAPTURE
+    it will open the built-in camera for capturing the image
+    */
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (camera_intent.resolveActivity(getPackageManager()) == null)
+            Log.d("ImplicitIntent", "Can't handle this!");
+        else
+            someActivityResultLauncher.launch(camera_intent);
     }
 }
